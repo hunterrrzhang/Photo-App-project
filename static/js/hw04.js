0af3114516fe415e047a409d212b709b645848a2
Hunter <hunter@joinbeni.com>
2022-03-07 20:44:42 -0600

    hw05 update

--- static/js/hw04.js
+++ static/js/hw04.js
@@ -1,3 +1,112 @@
+let textboxId;
+let targetId;
+let requestBody;
+let method;
+let pre;
+let url;
+    
+const sendRequest = ev => {
+    console.log(ev);
+    textboxId = ev.currentTarget.dataset.textbox;
+    targetId = ev.currentTarget.dataset.target;
+    requestBody = ev.currentTarget.dataset.request_body;
+    method = ev.currentTarget.dataset.method;
+    pre = document.getElementById(targetId);
+    url = document.getElementById(textboxId).value;
+    console.log(textboxId, targetId, method);
+    console.log(url);
+    if (method === 'get') {
+        sendGetRequest(pre, url)
+    } else if (['post', 'patch'].includes(method.toLowerCase())) {
+        let elem = document.getElementById(requestBody);
+        try {
+            const val =  elem.innerHTML.replace(/(<([^>]+)>)/gi, "");
+            let body = JSON.parse(val);
+            sendPostPatchRequest(pre, url, method, body);
+        } catch {
+            alert('Invalid JSON: ' + val);
+        }
+    } else if (method === 'delete') {
+        sendDeleteRequest(pre, url);
+    } else {
+        pre.innerHTML = 'Unrecognized method.';
+    }
+};
+
+const displayStatusCode = response => {
+    let elem = document.getElementById(textboxId + '-status-code');
+    elem.classList.remove('active');
+    elem.innerHTML = `${response.status} ${response.statusText}`;
+    hljs.highlightElement(elem);
+    setTimeout(() => {elem.classList.add('active')} , 0);
+};
+
+const displayResponse = (data, elem) => {
+    console.log(data);
+    // elem.classList.remove('active');
+    elem.innerHTML = JSON.stringify(data, null, 3);
+    hljs.highlightElement(elem);
+    setTimeout(() => {elem.classList.add('active')} , 1);
+};
+
+const sendGetRequest = (elem, url) => {
+    elem.classList.remove('active');
+    fetch(url)
+        .then(response => {
+            displayStatusCode(response);
+            return response.json()
+        })
+        .then(data => displayResponse(data, elem));
+};
+
+const sendPostPatchRequest = (elem, url, method, body) => {
+    elem.classList.remove('active');
+    fetch(url, {
+            method: method.toUpperCase(),
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': getCookie('csrf_access_token')
+            },
+            body: JSON.stringify(body),
+        })
+        .then(response => {
+            displayStatusCode(response);
+            return response.json()
+        })
+        .then(data => displayResponse(data, elem));
+};
+
+const sendDeleteRequest = (elem, url) => {
+    elem.classList.remove('active');
+    fetch(url, { 
+            method: 'DELETE',
+            headers: {
+                'X-CSRF-TOKEN': getCookie('csrf_access_token')
+            }
+        })
+        .then(response => {
+            displayStatusCode(response);
+            return response.json()
+        })
+        .then(data => displayResponse(data, elem));
+};
+
+const getCookie = key => {
+    let name = key + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
+
 const story2Html = story => {
     return `
         <div>
@@ -187,6 +296,7 @@ const followUser = (userID, elem) => {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': getCookie('csrf_access_token')
         },
         body: JSON.stringify(postData)
     })
@@ -206,7 +316,7 @@ const unfollowUser = (followingID, elem) => {
     fetch(deleteUrl, {
         method: "DELETE",
         headers: {
-            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': getCookie('csrf_access_token')
         }
     })
     .then(response => response.json())
@@ -286,6 +396,9 @@ const likePost = (postID, elem) => {
     const postData = {};
     fetch(`/api/posts/${postID}/likes/`, {
             method: "POST",
+            headers: {
+                'X-CSRF-TOKEN': getCookie('csrf_access_token')
+            },
             body: JSON.stringify(postData)
         })
         .then(response => response.json())
@@ -300,6 +413,9 @@ const likePost = (postID, elem) => {
 const unlikePost = (postID, elem) => {
     fetch(`/api/posts/${postID}/likes/${elem.dataset.likeId}`, {
         method: "DELETE",
+        headers: {
+            'X-CSRF-TOKEN': getCookie('csrf_access_token')
+        }
     })
     .then(response => response.json())
     .then(data => {
@@ -331,6 +447,7 @@ const bookmarkPost = (postID, elem) => {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': getCookie('csrf_access_token')
             },
             body: JSON.stringify(postData)
         })
@@ -346,6 +463,9 @@ const bookmarkPost = (postID, elem) => {
 const unbookmarkPost = (postID, elem) => {
     fetch(`/api/bookmarks/${elem.dataset.bookmarkId}`, {
         method: "DELETE",
+        headers: {
+            'X-CSRF-TOKEN': getCookie('csrf_access_token')
+        }
     })
     .then(response => response.json())
     .then(data => {
@@ -380,7 +500,7 @@ const commentPost = (e, form) => {
     fetch("/api/comments", {
             method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': getCookie('csrf_access_token')
             },
             body: JSON.stringify(postData)
         })
